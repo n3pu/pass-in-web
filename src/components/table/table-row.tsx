c7950161010241c20b477b22b202533678b93d42
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2024-04-03 16:28:25 -0300

    Grid list pagination

---
+++ src/components/table/table-row.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface TableRowProps extends ComponentProps<'tr'> {
+
+}
+
+export function TableRow(props: TableRowProps) {
+  return (
+    <tr {...props} className={twMerge('border-b border-white/10 hover:bg-white/5', props.className)} />
+  )
+}
